e6cf80bc97de27838ea6223a592e7e46fc8f4eaf
Sara Nabizadeh <121334610+saranabizadeh@users.noreply.github.com>
2026-05-12 10:55:09 -0500

    Delete 02_categories.sql

--- 02_categories.sql
+++
@@ -1,8 +0,0 @@
-SELECT pr.product_category_name,
-       SUM(i.price) AS revenue
-FROM order_items i
-JOIN products pr
-ON i.product_id = pr.product_id
-GROUP BY pr.product_category_name
-ORDER BY revenue DESC
-LIMIT 10;
